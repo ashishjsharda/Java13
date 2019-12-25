8b56b4eb20eaf85007efafa16bf3e3f9944396f3
Ashish S <ashishjsharda@gmail.com>
2019-12-25 08:48:31 -0500

    Create MultiBlockStrin.java

---
+++ MultiBlockStrin.java
@@ -0,0 +1,23 @@
+package JDK13;
+
+/**
+ * Multi Block String Java 13
+ * @author ashish
+ *
+ */
+public class MultiBlockString {
+
+	@SuppressWarnings("preview")
+	public static void main(String[] args) {
+
+	
+    String text="""
+    		This is first multiline string
+    		You can even write html code here
+    		""";
+    System.out.println(text);
+
+		
+	}
+
+}
