4d4b8a93ba4edc49f5ad14a695664c0077b15cd0
Ashish S <ashishjsharda@gmail.com>
2020-02-20 07:31:38 -0500

    Create StringTut.java

---
+++ StringTut.java
@@ -0,0 +1,19 @@
+package Java13;
+
+/**
+ * MultiLine String
+ * @author ashish
+ */
+public class StringTut {
+    public static void main(String[] args) {
+
+        String html= """
+                <html>
+                <h1>
+                Hello World
+                </h1>
+                </html>
+                """;
+        System.out.println(html);
+    }
+}
